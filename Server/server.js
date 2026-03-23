d827024ec94febee5da09ecb048f1e41c61d7831
sriram-kavadi <kavadi.sriram@gmail.com>
2026-03-23 12:34:28 +0530

    done api integration

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+
+const app=express();
+
+const port=9999;
+
+const cors=require("cors")
+const corsOptions={
+    origin:"http://localhost:5173"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.get("/api",(req,res)=>{
+    res.json({fruits:["apple","banana","orange"]})
+})
+
+app.get("/hey",(req,res)=>{
+    res.json({check:["nikhil","sriram"]})
+})
+
+app.listen(port,()=>{
+    console.log("listening..")
+})
